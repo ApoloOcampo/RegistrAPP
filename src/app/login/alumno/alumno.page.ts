4ec6000a201a2d5f21de722c4c7fe8e4596efb28
Apolo Ocampo Rios <apolo.ocamporios@gmail.com>
2023-09-11 22:07:37 -0300

    implementando validacion login alumno y docente

--- src/app/login/alumno/alumno.page.ts
+++ src/app/login/alumno/alumno.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alumno',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlumnoPage implements OnInit {
 
-  constructor() { }
+  user: string = '';
+  pass: string = '';
+
+  constructor(private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
+
+  validarAlumno() {
+    if (this.pass=='1234' && this.user=='ke.lagos' || this.pass=='1234' && this.user=='ap.ocampo') {
+      this.router.navigate(['/login/alumno/inicio-alumno']);
+     
+    } else {
+      this.mostrarAlerta();
+    }
+  }
+
+  async mostrarAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Usuario no encontrado, vuelva a intentarlo.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+  }
+
 
 }
--- src/app/login/docente/docente.page.ts
+++ src/app/login/docente/docente.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-docente',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocentePage implements OnInit {
 
-  constructor() { }
+  user: string = '';
+  pass: string = '';
+
+
+  constructor(private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
+
+
+  validarDocente() {
+    if (this.pass=='1234' && this.user=='danilo') {
+      this.router.navigate(['/login/docente/inicio-docente']);
+
+   
+     
+    } else {
+      this.mostrarAlerta();
+    }
+  }
+
+  async mostrarAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Usuario no encontrado, vuelva a intentarlo.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+  }
+
 
 }
